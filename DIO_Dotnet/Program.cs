7f7abfe928458f555e82b569276612bc94c98ac8
Rapha-Y <raph.yuki@gmail.com>
2020-06-08 21:06:26 -0300

    initial commit

---
+++ DIO_Dotnet/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DIO_Dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = 5;
+            
+            for(int i = 0; i < maxNumber; i++) {
+                Console.WriteLine($"Welcome to the .NET course {i}");
+            }
+
+        }
+    }
+}
