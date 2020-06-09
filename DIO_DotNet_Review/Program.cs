c1def9e21ebd679114890e01887a4ec0a1bb0cc6
Rapha-Y <raph.yuki@gmail.com>
2020-06-09 15:29:36 -0300

    added folder w/simple choice picker application

---
+++ DIO_DotNet_Review/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DIO_DotNet_Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose your desired option:");
+            Console.WriteLine("1. Insert new student");
+            Console.WriteLine("2. List students");
+            Console.WriteLine("3. Calculate grades average");
+            Console.WriteLine("X. Quit");
+            Console.WriteLine();
+
+            string userOption = Console.ReadLine();
+
+            while(userOption.ToUpper() != "X") {
+                switch(userOption) {
+                    case "1": 
+                        //TODO: insert student
+                        break;
+                    case "2":
+                        //TODO: list students
+                        break;
+                    case "3":
+                        //TODO: calc average grade
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                Console.WriteLine("Choose your desired option:");
+                Console.WriteLine("1. Insert new student");
+                Console.WriteLine("2. List students");
+                Console.WriteLine("3. Calculate grades average");
+                Console.WriteLine("X. Quit");
+                Console.WriteLine();
+
+                userOption = Console.ReadLine();
+            }
+        }
+    }
+}
